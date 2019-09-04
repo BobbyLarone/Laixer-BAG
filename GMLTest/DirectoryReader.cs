767a86627104c8aa8ee329b03701703d746b2434
Bobby <b.poeran@live.nl>
2019-09-04 15:50:42 +0200

    added read folder methods in DirectoryReader

--- GMLTest/DirectoryReader.cs
+++ GMLTest/DirectoryReader.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GMLTest
 {
@@ -9,5 +11,38 @@ namespace GMLTest
     /// </summary>
     class DirectoryReader
     {
+        private static readonly string fileDirectory = "";
+
+        public DirectoryReader()
+        {
+
+        }
+
+        public void readFolder(string filePath)
+        {
+
+            readFolderAsync(filePath);
+        }
+
+        private void readFolderAsync(string filePath)
+        {
+            if(Directory.Exists(filePath))
+            {
+                var listOfDirectories = Directory.EnumerateDirectories(filePath);
+                var listOfFiles = Directory.EnumerateFiles(filePath);
+
+                foreach(var path in listOfDirectories)
+                {
+                    Console.WriteLine($"Found: {path}");
+                }
+
+                Console.WriteLine("These are the files found in the current directory");
+
+                foreach (var file in listOfFiles)
+                {
+                    Console.WriteLine($"Found: {file}");
+                }
+            }
+        }
     }
 }
