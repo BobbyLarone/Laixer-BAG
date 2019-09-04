e415b127512411db2e216c36c86f5a7c2e32d661
Bobby <b.poeran@live.nl>
2019-09-04 14:56:22 +0200

    Created a new file called DirectoryReader

---
+++ GMLTest/DirectoryReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GMLTest
+{
+    /// <summary>
+    ///  Class to read the content of the specified directory
+    /// </summary>
+    class DirectoryReader
+    {
+    }
+}
