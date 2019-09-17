5f68d503403e2e0f3cd35ac711a0577756b597ce
Bobby <b.poeran@live.nl>
2019-09-17 16:27:12 +0200

    Modified Pipeline pattern for automatic extraction and insertion to database

--- GMLTest/Program.cs
+++ GMLTest/Program.cs
@@ -7,35 +7,63 @@ namespace LaixerGMLTest
 {
     internal sealed class Program
     {
-        private DirectoryReader directoryReader;
+        private static DirectoryReader directoryReader;
         private string path;
         private ILoader loader;
         private List<BAGObject> list;
+        private List<string> directories;
+
+        private static int fileCount = 0;
 
         private Program Extract(string path)
         {
             // TODO: Step 1
 
+            // Make a new directory reader
+            directoryReader = new DirectoryReader();
+            
+            // Store the path to this folder
             this.path = path;
+            
+            // First set the loader
+            directoryReader.SetLoader(loader);
+
+            // Then read the filepath
+            directoryReader.readFolder(path);
+            
+            // Adjust the directory to read from the specified directory
+            directoryReader.SetDirectoryNumber(2); // read the map : Openbare Ruimtes
+
+            // Get the amount of files in the directory
+            fileCount = directoryReader.GetFileCountInDirectory();
+
+            return this;
+        }
+        private Program Process(int fileNumber)
+        {
+            // TODO: Step2
+
+            // Get a list of directories from that filepath from above
+            directories = directoryReader.GetListOfDirectories();
+
+            var files = directoryReader.GetListOfFilesInDirectory();
+
+            // Generate a list of BAGObjects
+            directoryReader.ReadFile(files[fileNumber]);
+
+            // retrieve the list of BagObjects
+            list = directoryReader.GetAllObjects();
+
             return this;
         }
 
         private Program Load<TLoader>()
             where TLoader : ILoader, new()
         {
-            // TODO: Step 2
+            // TODO: Step 3
 
-            loader = new TLoader(); // TODO: Fow now
+            loader = new TLoader();
             loader.Load(list);
-            return this;
-        }
-
-        private Program Process()
-        {
-            // TODO: Run
-            directoryReader = new DirectoryReader(loader); // TODO: Fow now
-            directoryReader.readFolder(path);
-            list = directoryReader.GetAllObjects();
             return this;
         }
 
@@ -50,13 +78,24 @@ namespace LaixerGMLTest
             var timer = new Stopwatch();
             timer.Start();
 
-            var processedObjects = new Program()
-                .Extract(path: args[0])
-                .Process()
+            //var processedObjects = new Program()
+            //    .Extract(path: args[0])
+            //    .Process()
+            //    .Load<DatabaseLoader>();
+
+            // !!! This part does not fully follow the ETL pattern but comes close to it ?.. (>.<)
+
+            // run Extract once for now..
+            new Program().Extract(path: args[0]);
+            
+            for(int x = 0; x < fileCount;x++)
+            {
+                new Program().Process(x)
                 .Load<DatabaseLoader>();
+                Console.WriteLine($"Loaded file number {x}");
+            }
 
             timer.Stop();
-            Console.WriteLine($"Made {processedObjects} objects!");
             Console.WriteLine($"Read within {timer.Elapsed.TotalSeconds} seconds");
             Console.ReadLine();
         }
