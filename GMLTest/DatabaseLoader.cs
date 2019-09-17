04ca9fcf0afa9a7b200037c626c2c4661cd797ea
Bobby <b.poeran@live.nl>
2019-09-17 16:24:22 +0200

    Add Dapper queries to loadOPR

--- GMLTest/DatabaseLoader.cs
+++ GMLTest/DatabaseLoader.cs
@@ -14,9 +14,18 @@ namespace LaixerGMLTest
             {
                 // TOOD: Check to insert which table.
 
-                var residenceObjects = bAGObjects.Cast<Residence>();
+                //var residenceObjects = bAGObjects.Cast<Residence>();
 
-                var sql = $@"
+                var residenceObjects = bAGObjects.Cast<PublicSpace>();
+
+                var sql = LoadOPR();
+
+                var orderDetails = connection.Execute(sql, residenceObjects);
+            }
+        }
+        private string LoadWPL()
+        {
+            return $@"
                     INSERT INTO public.woonplaats(
                         identificatie,
                         aanduidingrecordinactief,
@@ -45,9 +54,65 @@ namespace LaixerGMLTest
                         @WoonplaatsStatus::woonplaatsstatus,
                         null,
                         ST_Multi(ST_GeomFromGML(@Geovlak, 28992)))";
+        }
+        private string LoadOPR()
+        {
+            return @"INSERT INTO public.openbareruimte(
+                        identificatie,
+                        aanduidingrecordinactief,
+                        aanduidingrecordcorrectie,
+                        officieel,
+                        inonderzoek,
+                        begindatumtijdvakgeldigheid,
+                        einddatumtijdvakgeldigheid,
+                        documentnummer,
+                        documentdatum,
+                        openbareruimtenaam,
+                        openbareruimtestatus,
+                        openbareruimtetype,
+                        gerelateerdewoonplaats,
+                        verkorteopenbareruimtenaam)
+                        VALUES
+                        (
+                        @Identification,
+                        @AanduidingRecordInactief::boolean,
+                        @AanduidingRecordCorrectie::int,
+                        @Officieel::boolean,
+                        @InOnderzoek::boolean,
+                        @BegindatumTijdvakGeldigheid::timestamptz,
+                        @EinddatumTijdvakGeldigheid::timestamptz,
+                        @DocumentNummer,
+                        @DocumentDatum::date,
+                        @OpenbareRuimteNaam,
+                        @OpenbareruimteStatus::openbareruimtestatus,
+                        @OpenbareRuimteType::openbareruimtetype,
+                        @GerelateerdeWoonplaats,
+                        @VerkorteOpenbareruimteNaam)";
+        }
 
-                var orderDetails = connection.Execute(sql, residenceObjects);
-            }
+        private void LoadLIG()
+        {
+
+        }
+
+        private void LoadNUM()
+        {
+
+        }
+
+        private void LoadPND()
+        {
+
+        }
+
+        private void LoadSTA()
+        {
+
+        }
+
+        private void LoadVBO()
+        {
+
         }
     }
 }
